77e8bbe6159aaf03fadafbf91bcd23117a019cba
hectorarjr <hectorarjr@gmail.com>
2014-02-18 01:46:22 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -32,7 +32,7 @@ var MapsLib = {
   //example: locationColumn:     "'my location'",
   locationColumn:     "techaddress",
 
-  map_centroid:       new google.maps.LatLng(39.7392, -104.9847), //center that your map defaults to
+  map_centroid:       new google.maps.LatLng(41.8781136, -87.66677856445312), //center that your map defaults to
   locationScope:      "united states",      //geographical area appended to all address searches
   recordName:         "result",       //for showing number of results
   recordNamePlural:   "results",
