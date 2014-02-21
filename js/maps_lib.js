6fba1f7c9a2098f9c879b635eba73d6390bfcf61
hectorarjr <hectorarjr@gmail.com>
2014-02-21 00:25:40 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -69,7 +69,7 @@ var MapsLib = {
     var loadRadius = MapsLib.convertToPlainString($.address.parameter('radius'));
     if (loadRadius != "") $("#search_radius").val(loadRadius);
     else $("#search_radius").val(MapsLib.searchRadius);
-    $(":checkbox").prop("checked", "checked");
+    $(":checkbox").prop("checked", "unchecked");
     $("#result_box").hide();
     
     //-----custom initializers-------
@@ -91,22 +91,22 @@ var MapsLib = {
     
     var type_column1 = "'ni'";
     var tempaWhereClause = [];
-    if ( $("#ni").is(':checked')) tempaWhereClause.push("ni");
+    if ( $("#ni").is(':checked')) tempaWhereClause.push("ni"), tempaWhereClause.push(null);
     whereClause += " AND " + type_column1 + " IN ('" + tempaWhereClause.join("','") + "')";
     
     var type_column2 = "'voice'";
     var tempbWhereClause = [];
-    if ( $("#voice").is(':checked')) tempbWhereClause.push("voice");
+    if ( $("#voice").is(':checked')) tempbWhereClause.push("voice"), tempaWhereClause.push(null);
     whereClause += " AND " + type_column2 + " IN ('" + tempbWhereClause.join("','") + "')";
     
     var type_column3 = "'av'";
     var tempcWhereClause = [];
-    if ( $("#av").is(':checked')) tempcWhereClause.push("av");
+    if ( $("#av").is(':checked')) tempcWhereClause.push("av"), tempaWhereClause.push(null);
     whereClause += " AND " + type_column3 + " IN ('" + tempcWhereClause.join("','") + "')";
     
     var type_column4 = "'dmb'";
     var tempdWhereClause = [];
-    if ( $("#dmb").is(':checked')) tempdWhereClause.push("dmb");
+    if ( $("#dmb").is(':checked')) tempdWhereClause.push("dmb"), tempaWhereClause.push(null);
     whereClause += " AND " + type_column4 + " IN ('" + tempdWhereClause.join("','") + "')";
     
 
