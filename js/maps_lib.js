c02b523c1d5cf73942cfc58b3af07ebf53ccf513
hectorarjr <hectorarjr@gmail.com>
2014-02-27 00:57:01 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -337,26 +337,6 @@ getPointDistances: function(json) {
 },
 
 pointDistanceCallback: function(response, status) {
-   if (status != google.maps.DistanceMatrixStatus.OK) {
-    alert('Error was: ' + status);
-    } else {
-    var origins = response.originAddresses;
-    var destinations = response.destinationAddresses;
-    var outputDiv = document.getElementById('outputDiv');
-    outputDiv.innerHTML = '';
-    deleteOverlays();
-     
-    for (var i = 0; i < origins.length; i++) {
-      var results = response.rows[i].elements;
-      // addMarker(origins[i], false);
-      for (var j = 0; j < results.length; j++) {
-      // addMarker(destinations[j], true);
-      outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
-      + ': ' + results[j].distance.text + ' in '
-      + results[j].duration.text + '<br>';
-      }
-    }
-    }
   // See Parsing the Results for
   // the basics of a callback function.
   // https://developers.google.com/maps/documentation/javascript/distancematrix#distance_matrix_parsing_the_results
