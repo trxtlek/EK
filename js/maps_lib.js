c29663a935792ccc0ec0c66c7432cd286e941123
Trxtl <hughodesk@gmail.com>
2014-03-14 12:02:32 -0600

    Results column changed.
    Trying to get my result count to pick up the column with info. to be displayed. Currently, there should be about 2496 results and none are showing.

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -30,7 +30,7 @@ var MapsLib = {
   //name of the location column in your Fusion Table.
   //NOTE: if your location column name has spaces in it, surround it with single quotes
   //example: locationColumn:     "'my location'",
-  locationColumn:     "city/state/zip",
+  locationColumn:     "city_state_zip",
 
   map_centroid:       new google.maps.LatLng(39.50, -98.35), //center that your map defaults to
   locationScope:      "",      //geographical area appended to all address searches
@@ -271,7 +271,7 @@ var MapsLib = {
   //------results list-----
   
   getList: function(whereClause) {
-  var selectColumns = "tech, state/city/zip ";
+  var selectColumns = "tech, state_city_zip ";
   MapsLib.query(selectColumns, whereClause, "MapsLib.displayList");
   },
   
