485d10b92873a786d821fe43f26184f454f3cf24
Trxtl <hughodesk@gmail.com>
2014-03-24 14:27:53 -0600

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -75,6 +75,44 @@ var MapsLib = {
     //-----custom initializers-------
     
     
+    //-----distance calculator-------
+    
+    Maps.Lib.calculateDistances ();
+    function calculateDistances () {
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [origin1, origin2],
+          destinations: [destinationA, destinationB],
+          travelMode: google.maps.Travelmode.DRIVING,
+          unitSystem: google.maps.UnitSystem.METRIC,
+          avoidHighways: false,
+          avoidTolls: false
+        }, callback);
+    }
+    
+    function callback(response, status) {
+      if (status != google.maps.DistanceMatrixStatus.OK) {
+        alert ('Error was: ' + status);
+      } else {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+        var outputDiv = document.getElementById('outputDiv');
+        outputDiv.innerHTML = '';
+        deleteOverlays();
+      
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows [0].elements;
+          addMarker(origins[0], false);
+          for (var j = 0; j < results.length; j++) {
+            addMarker(destinations[j], true);
+            outputDiv.innerHTML += origins [0] + ' to ' + destinations[j]
+                + '; ' + results[j] .distance.test+ ' in'
+                + results[j].duration.text + '<br>';
+          }     
+        }
+      }
+    }
     //-----end of custom initializers-------
 
     //run the default search
